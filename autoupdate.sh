bd19a807959c16ed48a5e18cbde43ebc9b939dd3
= <=>
2017-05-16 16:26:24 +0100

    Added slack notificatino

--- autoupdate.sh
+++ autoupdate.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
-FileYouWantToTest='update.txt'
+FileYouWantToTest='/docker/homeassistant/update.txt'
 echo -n "Verifying if $FileYouWantToTest exist..."
 if [ -f $FileYouWantToTest ]
     then
         rm -f $FileYouWantToTest
         ./pull.sh
+	
+	cd /docker/homeassistant
+	echo -n "Pulling from Git"
+	git pull origin master
+ 	echo -n "Restarting docker"
+	docker restart home-assistant
 fi
 exit 0
