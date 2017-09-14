4e9f44932035ce4f25d103317a41830ec695e566
Ross Dargan <rossdargan@users.noreply.github.com>
2017-09-14 07:39:39 +0100

    Update monzo.py

--- custom_components/sensor/monzo.py
+++ custom_components/sensor/monzo.py
@@ -219,7 +219,7 @@ class MonzoAuthCallbackView(HomeAssistantView):
 
 class MonzoSensor(Entity):
     """Representation of a Sensor."""
-    from monzo.monzo import Monzo
+    import monzo.monzo
     
     def __init__(self, oauth, name):
         self._name = name
@@ -247,7 +247,7 @@ class MonzoSensor(Entity):
             token_refreshed = True
         if self._client is None or token_refreshed:
             self._client = \
-                Monzo(self._token_info.get('access_token'))
+                monzo.Monzo(self._token_info.get('access_token'))
             self._account_id = self._client.get_first_account()['id']
 
     @property
